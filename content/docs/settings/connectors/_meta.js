3b0e36ee45c778d81ee88355a84757fff0e80528
Tiberio Zolzettich <tzolzett@live.fr>
2025-10-12 22:54:12 +0200

    add : bigquery

--- content/docs/settings/connectors/_meta.js
+++ content/docs/settings/connectors/_meta.js
@@ -1,6 +1,7 @@
 import { newLabel } from "@components/new_label";
 
 export default {
+  bigquery: <div key="bigquery">BigQuery {newLabel}</div>,
   microsoft: "Microsoft",
   notion: <div key="notion">Notion {newLabel}</div>,
 };
