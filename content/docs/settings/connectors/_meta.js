4b555044a24777eca9a7aded54fef20191fdc2b3
Tiberio Z. <131868034+TiberioZ@users.noreply.github.com>
2025-11-04 00:29:55 +0100

    Release/v2025.11.0 (#14)
    * add : Notion connector
    * add : Bigquery connector

--- content/docs/settings/connectors/_meta.js
+++ content/docs/settings/connectors/_meta.js
@@ -1,5 +1,7 @@
-import { newLabel } from '@components/new_label';
+import { newLabel } from "@components/new_label";
 
 export default {
-    microsoft: 'Microsoft',
-}
+  bigquery: <div key="bigquery">BigQuery {newLabel}</div>,
+  microsoft: "Microsoft",
+  notion: <div key="notion">Notion {newLabel}</div>,
+};
