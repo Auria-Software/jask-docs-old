4823948877627b46227a97231daca81603e2a9ae
Tiberio Zolzettich <tzolzett@live.fr>
2026-01-09 18:29:05 +0100

    google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,51 +1,66 @@
-import { Footer, Layout, Navbar } from 'nextra-theme-docs'
-import { Banner, Head } from 'nextra/components'
-import { getPageMap } from 'nextra/page-map'
-import 'nextra-theme-docs/style.css'
+import { Footer, Layout, Navbar } from "nextra-theme-docs";
+import { Banner, Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+import "nextra-theme-docs/style.css";
+import Script from "next/script";
 
-
-
-const banner = <Banner storageKey="2.0-release">
-        <a href="/docs/applications/create/actions/file_processing">
-          🎉 Nouvelles actions de traitement de fichiers dans les applications →
-        </a>
-      </Banner>
+const banner = (
+  <Banner storageKey="2.0-release">
+    <a href="/docs/applications/create/actions/file_processing">
+      🎉 Nouvelles actions de traitement de fichiers dans les applications →
+    </a>
+  </Banner>
+);
 const navbar = (
-    <Navbar
-        logo={<b><img src="/logo_jask.png" alt="Logo Jask" width={100} height={100}/></b>}
+  <Navbar
+    logo={
+      <b>
+        <img src="/logo_jask.png" alt="Logo Jask" width={100} height={100} />
+      </b>
+    }
     // ... Your additional navbar options
-    />
-)
-const footer = <Footer>MIT {new Date().getFullYear()} © Jask.</Footer>
-
-
+  />
+);
+const footer = <Footer>MIT {new Date().getFullYear()} © Jask.</Footer>;
 
 export default async function RootLayout({ children }) {
-    return (
-        <html
-            // Not required, but good for SEO
-            lang="en"
-            // Required to be set
-            dir="ltr"
-            // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
-            suppressHydrationWarning
-        >
-            <Head>
-            </Head>
-            <body>
-                <Layout
-                    sidebar={{
-                        defaultMenuCollapseLevel: 1,
-                    }}
-                    banner={banner}
-                    navbar={navbar}
-                    pageMap={await getPageMap()}
-                    footer={null}
-                    docsRepositoryBase='https://github.com/Auria-Software/jask-docs/tree/main'
-                >
-                    {children}
-                </Layout>
-            </body>
-        </html>
-    )
+  return (
+    <html
+      // Not required, but good for SEO
+      lang="en"
+      // Required to be set
+      dir="ltr"
+      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+      suppressHydrationWarning>
+      <Head>
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VHWH82GG1F"
+        />
+        <Script id="google-analytics">
+          {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-VHWH82GG1F');
+            `}
+        </Script>
+      </Head>
+      <body>
+        <Layout
+          sidebar={{
+            defaultMenuCollapseLevel: 1,
+          }}
+          banner={banner}
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          footer={null}
+          docsRepositoryBase="https://github.com/Auria-Software/jask-docs/tree/main">
+          {children}
+        </Layout>
+      </body>
+    </html>
+  );
 }
