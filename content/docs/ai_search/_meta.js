0eb57a9b624319670eb61be03dc6390f3518aafd
Tiberio Zolzettich <tzolzett@live.fr>
2026-02-13 19:34:44 +0100

    fix

--- content/docs/ai_search/_meta.js
+++ content/docs/ai_search/_meta.js
@@ -1,8 +1,8 @@
 import { newLabel, betaLabel } from "@components/new_label";
+
 export default {
-    intro: "Vue d'ensemble",
-    personnal: "Mes bibliothèques",
-    search: "Modèles de document {betaLabel}",
-    workbooks: "Carnets de travail {betaLabel}",
-    history : "Historique {newLabel}"
-}
+  intro: "Vue d'ensemble",
+  personnal: "Mes bibliothèques",
+  search_home: <div key="search_home">Modèles de document {betaLabel}</div>,
+  workbooks: <div key="workbooks">Carnets de travail {betaLabel}</div>,
+};
