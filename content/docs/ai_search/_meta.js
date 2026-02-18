4ccdf50c9cf3f798a9b274d3f22644d5b5434115
anatbr <anatolebrillion@gmail.com>
2026-02-18 15:01:07 +0100

    Update _meta.js

--- content/docs/ai_search/_meta.js
+++ content/docs/ai_search/_meta.js
@@ -3,6 +3,5 @@ import { newLabel, betaLabel } from "@components/new_label";
 export default {
   intro: "Vue d'ensemble",
   personnal: "Mes bibliothèques",
-  search_home: <div key="search_home">Modèles de document {betaLabel}</div>,
   workbooks: <div key="workbooks">Carnets de travail {betaLabel}</div>,
 };
