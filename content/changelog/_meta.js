7acb7faba61df8a7bab96ef15e17d363b6bd92be
anatbr <anatolebrillion@gmail.com>
2026-02-19 18:40:16 +0100

    Update _meta.js

--- content/changelog/_meta.js
+++ content/changelog/_meta.js
@@ -6,6 +6,5 @@ export default {
     title: "Version",
   },
     version202602: <div key="202602">v.2026.02 {newLabel}</div>,
-    version202511: <div key="202511">v.2025.11</div>,
     version202510: <div key="202510">v.2025.10</div>,
 };
