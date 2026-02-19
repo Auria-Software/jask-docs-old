6821369493aacdfba4464eedca94b0c38d46f0aa
anatbr <anatolebrillion@gmail.com>
2026-02-19 12:28:32 +0100

    Create _meta.js

---
+++ content/docs/agentia/applications/create/actions/_meta.js
@@ -0,0 +1,14 @@
+import { newLabel, betaLabel } from "@components/new_label";
+
+export default {
+    context: "Contexte",
+    prompts_store: "Prompts Store",
+    file_processing: <div key="file_processing">Traitement de fichiers {newLabel}</div>,
+    assistants: "Agents",
+    data_sources: "Sources de Données",
+    document_production: "Production de Documents",
+    web: <div key="web">Web {betaLabel}</div>,
+    m365: "Microsoft 365",
+    task: "Tâches prédéfinies",
+    control_structure: "Structures de Contrôle (avancé)",
+}
