f3f2d9153d81ee7bf68294c02ac6f01978abb9bd
jules7lemaire-ui <jules7.lemaire@gmail.com>
2026-02-20 10:39:57 +0100

    Update _meta.js

--- content/docs/ai_search/_meta.js
+++ content/docs/ai_search/_meta.js
@@ -1,7 +1,7 @@
 import { newLabel, betaLabel } from "@components/new_label";
 
 export default {
-  intro: "Vue d'ensemble",
+  intro: "Introduction",
   personnal: "Mes bibliothèques",
   workbooks: <div key="workbooks">Carnets de travail {betaLabel}</div>,
 };
