e8b0359f60e6fcc2ff7e173957a3ef7a5ab1ea9b
Tiberio Zolzettich <tzolzett@live.fr>
2026-02-22 11:42:22 +0100

    fix

--- content/docs/ai_search/_meta.js
+++ content/docs/ai_search/_meta.js
@@ -2,6 +2,5 @@ import { newLabel, betaLabel } from "@components/new_label";
 
 export default {
   intro: "Introduction",
-  personnal: "Mes bibliothèques",
-  workbooks: <div key="workbooks">Carnets de travail {betaLabel}</div>,
+  features: "Fonctionnalités",
 };
