8c8f9dbb31f894004c787bac074d5268fb3e62a9
anatbr <anatolebrillion@gmail.com>
2026-04-20 19:05:38 +0200

    Update _meta.js

--- content/docs/datasources/connectors/providers/_meta.js
+++ content/docs/datasources/connectors/providers/_meta.js
@@ -4,7 +4,7 @@ export default {
   google: "Google",
   microsoft: "Microsoft",
   notion: "Notion",
-  odoo: <NewLabel>Odoo</NewLabel>,
-  RSS: <NewLabel>RSS</NewLabel>,
-  salesforce: <NewLabel>Salesforce</NewLabel>,
+  odoo: <div key="odoo">"Odoo" {NewLabel}</div>,
+  RSS: <div key="rss">"RSS" {NewLabel}</div>,
+  salesforce: <div key="salesforce">"Salesforce" {NewLabel}</div>,
 };
