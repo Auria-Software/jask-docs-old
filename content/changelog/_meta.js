f8fd18d9b5d83bc0504b34a7ffb6c12e7b3c7308
anatbr <anatolebrillion@gmail.com>
2026-04-20 18:55:23 +0200

    Update _meta.js

--- content/changelog/_meta.js
+++ content/changelog/_meta.js
@@ -1,9 +1,11 @@
+import { NewLabel } from "@components/new_label";
+
 export default {
   _1: {
     type: "separator",
     title: "Version",
   },
-  version202604: <div key="202604">"v.2026.04" {newLabel}</div>,
+  version202604: <div key="202604">"v.2026.04" {NewLabel}</div>,
   version202602: "v.2026.02",
   version202510: "v.2025.10",
 };
