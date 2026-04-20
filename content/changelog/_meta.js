b2d312dbde3727632168e2453da56ecafb9249e3
anatbr <anatolebrillion@gmail.com>
2026-04-20 14:15:48 +0200

    Add new version entry for 2026.04

--- content/changelog/_meta.js
+++ content/changelog/_meta.js
@@ -3,6 +3,7 @@ export default {
     type: "separator",
     title: "Version",
   },
+  version202604: <div key="202604">"v.2026.04" {newLabel}</div>,
   version202602: "v.2026.02",
   version202510: "v.2025.10",
 };
