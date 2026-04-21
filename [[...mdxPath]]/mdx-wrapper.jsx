9007d573144db26acf93dad65fff6de208d9889a
anatbr <anatolebrillion@gmail.com>
2026-04-21 10:40:28 +0200

    Delete [[...mdxPath]]/mdx-wrapper.jsx

--- [[...mdxPath]]/mdx-wrapper.jsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { useMDXComponents as getMDXComponents } from '../../mdx-components'
-
-export function MDXWrapper({ metadata, children }) {
-  const { wrapper: Wrapper } = getMDXComponents()
-  return <Wrapper metadata={metadata}>{children}</Wrapper>
-}
