1f999135397293900cf2b1cab140e9bfa8fb71b2
anatbr <anatolebrillion@gmail.com>
2026-04-21 09:41:53 +0200

    Add new emoji to version 2026.04

--- content/changelog/_meta.js
+++ content/changelog/_meta.js
@@ -3,7 +3,7 @@ export default {
     type: "separator",
     title: "Version",
   },
-  version202604: "v.2026.04",
+  version202604: "v.2026.04 🆕",
   version202602: "v.2026.02",
   version202510: "v.2025.10",
 };
