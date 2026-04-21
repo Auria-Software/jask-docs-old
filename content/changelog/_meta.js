7028e76e3d92cdbbaed6214d4a4a1fd0d05498bb
anatbr <anatolebrillion@gmail.com>
2026-04-21 13:51:36 +0200

    Update version label to use NewLabel component

--- content/changelog/_meta.js
+++ content/changelog/_meta.js
@@ -1,9 +1,11 @@
+import { NewLabel } from "@components/new_label";
+
 export default {
   _1: {
     type: "separator",
     title: "Version",
   },
-  version202604: "v.2026.04 🆕",
+  version202604: <NewLabel>v.2026.04</NewLabel>,
   version202602: "v.2026.02",
   version202510: "v.2025.10",
 };
